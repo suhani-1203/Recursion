df2501455c078ab41fe38133fb5762c38c89ccb0
suhani-1203 <sagarwal1_be22@thapar.edu>
2024-10-23 14:46:05 +0530

    Time: 1521 ms (5.02%), Space: 515.6 MB (5%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> generateCombinations(int index,vector<int> &candidates,int target,vector<int> &current,vector<vector<int>> &ans){
+        if(target<0 ||index==candidates.size()){
+            return ans;
+        }
+        if(target==0){
+            ans.push_back(current);
+            return  ans;
+        }
+        current.push_back(candidates[index]);
+        target-=candidates[index];
+        generateCombinations(index,candidates,target,current,ans);
+        
+        current.pop_back();
+        target+=candidates[index];
+        generateCombinations(index+1,candidates,target,current,ans);
+
+        return ans;
+
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> current;
+        generateCombinations(0,candidates,target,current,ans);
+        return ans;
+
+    }
+};
